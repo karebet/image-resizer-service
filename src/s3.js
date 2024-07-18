d3547ecc69e4fb4d4c9a00bd48d17f964dfcc867
karebetconnec <karebetconnec@gmail.com>
2024-07-18 06:29:01 +0000

    upgrade to node20.x

--- src/s3.js
+++ src/s3.js
@@ -1,5 +1,5 @@
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
+import { S3Client } from "@aws-sdk/client-s3"
+const s3 = S3Client();
 
 exports.getFileFromBucket = (bucket, key) => s3.getObject({
     Bucket: bucket,
